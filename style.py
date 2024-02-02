737cd955e73def82364aecab781bcb6dd2e640ef
David Yeh <davidmilk880330@gmail.com>
2024-02-02 16:33:19 +0800

    add new file

---
+++ style.py
@@ -0,0 +1,18 @@
+# 好的寫法
+
+＃ coding style 寫程式的"風格"
+
+x = 5
+
+# pep 8 style guide for python
+ print('hi', x)
+y = 10
+height = 179
+my_name = 'allen'
+name_1 = 'tom'
+name_2 = 'David'
+# 註解就是寫給自己看的 不執行
+# 這裡在練習x = 5
+x = 5 # 把x的值設成5
+
+# 介紹比較符號
